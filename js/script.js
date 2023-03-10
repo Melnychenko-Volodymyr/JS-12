a840bca05941837c02e3c7ab9ab4e46cc39e828e
Melnychenko-Volodymyr <mvi_1968@i.ua>
2023-03-10 21:33:08 +0200

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+let cont = document.querySelector('.container');
 let img = document.querySelector('.picture');
 
 const pos = () => {
-  let x = 50 + Math.random() * 1100;
-  let y = 50 + Math.random() * 700;   
+
+  let x = Math.random() * (cont.clientWidth-50);
+  let y = Math.random() * (cont.clientHeight-50);   
  img.style.cssText = `top: ${y}px; left: ${x}px`;
 }
 
